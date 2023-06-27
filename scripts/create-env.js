a2793a707dc0720d3db6f8344138ea1721e97b2d
diegohp <diego.blake11@gmail.com>
2023-06-26 19:02:19 -0600

    [FIX-2] create-env

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 fs.writeFileSync(
   './.env',
-  `API_URL=${process.env.API_URL_PROD}\n
+  `API_URL=${process.env.API_URL}\n
   API_VERSION=${process.env.API_VERSION}\n
   API_KEY=${process.env.API_KEY}\n`
 );
